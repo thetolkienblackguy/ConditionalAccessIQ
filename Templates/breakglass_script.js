7ee62cd7d5a3b6518114b09bb6556c16f8d1a69e
thetolkienblackguy <28851692+thetolkienblackguy@users.noreply.github.com>
2025-03-10 17:19:06 -0400

    Create breakglass_script.js

---
+++ Templates/breakglass_script.js
@@ -0,0 +1,29 @@
+// Tab functionality
+function openTab(evt, tabName) {
+    // Declare variables
+    var i, tabcontent, tablinks;
+    
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the first tab by default when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the first tab button and click it
+    if (document.getElementsByClassName("tablinks").length > 0) {
+        document.getElementsByClassName("tablinks")[0].click();
+    }
+});
